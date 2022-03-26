71f5a24e761cd973eebb1c0ba79cdb9d7080a740
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-03-25 18:09:20 -0600

    add filter buttons

--- is-it-foam/src/ImageGallery.js
+++ is-it-foam/src/ImageGallery.js
@@ -17,9 +17,44 @@ const tagButton = () => {
 
 function ImageGallery() {
   const [displayCount, setDisplayCount] = useState(6);
+  const [filter, setFilter] = useState('no filter');
+
+  const changeFilter = (e) => {
+    setFilter(e.target.value);
+  }
 
   return (
     <>
+      <div onChange={changeFilter}>
+        <input
+          type="radio"
+          value="no filter"
+          name="filter"
+          checked={filter === "no filter"}
+        />
+        no filter
+        <input
+          type="radio"
+          value="unclassified"
+          name="filter"
+          checked={filter === "unclassified"}
+        />
+        unclassified
+        <input
+          type="radio"
+          value="foaming"
+          name="filter"
+          checked={filter === "foaming"}
+        />
+        foaming
+        <input
+          type="radio"
+          value="not foaming"
+          name="filter"
+          checked={filter === "not foaming"}
+        />
+        not foaming
+      </div>
       {reactor.data.slice(0, displayCount).map((reactor, idx) => {
         return <img style={imageStyle} src={reactor.url} alt='reactor image' key={idx} />
       })}
